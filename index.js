5f21f8c59ff39980a5010f65dac9d6765fdbf63b
Zac <zacharyflynn06@gmail.com>
2021-05-23 18:07:41 -0600

    commit tree 001a3187198b69b9eeffade93651fdb3f5eb121f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(-2)
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (surge) => {
+    return function (cost) {
+        return cost * surge
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (driversArray, driversFunction) => {
+    return driversFunction(driversArray)
+}
